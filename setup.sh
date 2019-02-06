c9cbb80b8725427c986c64b456087f8e7df67a66
Neil Lambeth <32801935+NeilLambeth@users.noreply.github.com>
2019-02-06 22:34:52 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,23 +11,12 @@ sudo apt-get install python3-dev python-dev python-pip python3-pip joystick -y
 sudo pip3 install evdev
 sudo pip install evdev
 
-sudo apt-get install build-essential git scons swig -y
+sudo apt-get install build-essential git -y
 
 sudo rm -rf RedBoard
 git clone https://github.com/RedRobotics/RedBoard.git
-cd RedBoard
-cp * /home/pi
-
-cd
-git clone https://github.com/jgarff/rpi_ws281x.git
-cd rpi_ws281x
-scons
-
-cd python
-sudo python setup.py install
-sudo python3 setup.py install
-
-
+cd Red
+#cp * /home/pi
 
 cd
 
@@ -37,15 +26,7 @@ then
 else
     echo "Installing shutdown script"
     cd
-    rm pigpio.zip
-    sudo rm -rf PIGPIO
-    wget abyz.co.uk/rpi/pigpio/pigpio.zip
-    unzip pigpio.zip
-    cd PIGPIO   
-    make
-    sudo make install
-    cd
-    rm pigpio.zip
+    sudo apt-get install pigpio python-pigpio python3-pigpio
     sudo sed -i -e '$i #start Pigpio deamon\nsudo pigpiod\n' /etc/rc.local
 fi
 
@@ -58,5 +39,5 @@ else
     sudo sed -i -e '$i #start reset_shutdown script\nsudo python3 /home/pi/ip.py; sudo python3 /home/pi/reset_shutdown.py&' /etc/rc.local
 fi
 
-sudo reboot
+#sudo reboot
 
