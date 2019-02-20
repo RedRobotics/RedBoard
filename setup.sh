4cd1c377ef27e6cae87bc32b04cb44733ef28b10
NeilLambeth <neillambeth@gmail.com>
2019-02-20 13:35:02 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,17 +14,30 @@ sudo apt-get install build-essential git -y
 
 sudo apt-get install pigpio python-pigpio python3-pigpio
 
-sudo rm -rf RedBoard
-git clone https://github.com/RedRobotics/RedBoard.git
 
-sudo systemctl enable pigpiod
+
+#sudo systemctl enable pigpiod
+
+if grep -Fq "pigpiod" "/etc/rc.local"
+then
+    echo "Pigpio already installed"
+else
+    echo "Installing shutdown script"
+    cd
+    sudo rm -rf RedBoard
+    git clone https://github.com/RedRobotics/RedBoard.git
+    #wget https://github.com/RedRobotics/RedBoard/archive/master.zip
+    sudo sed -i -e '$i #start Pigpio deamon\nsudo pigpiod\n' /etc/rc.local
+fi
+
+
 
 if grep -Fq "reset_shutdown.py" "/etc/rc.local"
 then
     echo "Shutdown script already running"
 else
     echo "Installing shutdown script" 
-    sudo sed -i -e '$i #start reset_shutdown script\nsudo python3 /home/pi/ip.py; sudo python3 /home/pi/reset_shutdown.py&' /etc/rc.local
+    sudo sed -i -e '$i #start reset_shutdown script\nsudo python3 /home/pi/RedBoard/ip.py; sudo python3 /home/pi/RedBoard/system_monitor.py&' /etc/rc.local
 fi
 
 #sudo reboot
