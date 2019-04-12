d45713d374fff9a1e3e8b08f303d733dc9d050e6
NeilLambeth <neillambeth@gmail.com>
2019-04-12 15:09:15 +0100

    Update allstop

--- allstop.py
+++ allstop.py
@@ -2,7 +2,19 @@ from redboard import *
 
 M1(0)
 M2(0)
-servo0_off()
-servo1_off()
+servo7_off()
+servo8_off()
+servo9_off()
+servo10_off()
+servo11_off()
 
+servo5_off()
+servo6_off()
+servo13_off()
+servo27_off()
+
+servo20_off()
+servo21_off()
+servo22_off()
+servo12_off()
 
