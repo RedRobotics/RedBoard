48ec5b67b55d2b87860801d569ed3ea093450978
NeilLambeth <neillambeth@gmail.com>
2019-11-27 20:41:49 +0000

    Update bat_check.py

--- system/bat_check.py
+++ system/bat_check.py
@@ -3,21 +3,25 @@
 
 import smbus
 import time
-import os
+
+ADC_bat_conversion_Value = 1098.0
+
 
 bus = smbus.SMBus(1)
 address = 0x48
 
 def readAdc_0():
     bus.write_i2c_block_data(address, 0x01, [0xc3, 0x83])
-    time.sleep(0.1)
+    time.sleep(0.01)
     adc = bus.read_i2c_block_data(address,0x00,2)
     return adc
 
 voltage0 = readAdc_0()
 
 # Battery Voltage
+
 conversion_0 = (voltage0[1])+(voltage0[0]<<8)
-volts_0 = conversion_0 / 1116 #  Battery voltage through voltage divider
+volts_0 = conversion_0 / ADC_bat_conversion_Value
+
 
 print (round(volts_0,2))
