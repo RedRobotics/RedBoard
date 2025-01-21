4a1a03387b9e2d9fad20098ef0c1f3f569e3e26d
Neil Lambeth <32801935+NeilLambeth@users.noreply.github.com>
2025-01-21 21:32:20 +0000

    Add files via upload

--- system/install.sh
+++ system/install.sh
@@ -1,17 +1,10 @@
-sudo apt-get install build-essential git scons swig -y
+#!/bin/bash
 
-git clone https://github.com/RedRobotics/RedBoard.git
-
+echo "Pi4 Install "
 sudo apt-get install python3-dev python3-pip joystick -y
-
 sudo pip3 install evdev
-
 sudo apt-get install python3-smbus i2c-tools -y
-
 sudo apt-get install pigpio python3-pigpio -y
-
 sudo pip3 install adafruit-circuitpython-ssd1306
-
 sudo apt-get install python3-pil
-
 sudo cp /home/pi/RedBoard/system/rc.local /etc/rc.local
